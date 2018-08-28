00996a63627f8eb056b7da24fd9fe8d710d4d456
Kyle <ssjmetroid@gmail.com>
2018-08-28 18:04:49 -0400

    Created a window that plots recent frame times to monitor performance.

--- march-squares/main.cpp
+++ march-squares/main.cpp
@@ -2,11 +2,13 @@
 #include <SFML/Graphics.hpp>
 #include "imgui.h"
 #include "imgui-SFML.h"
+#include <vector>
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
 	ImGui::SFML::Init(window);
 	sf::Clock clockImgui;
+	std::vector<float> frameTimes(500, 0.f);
 	while (window.isOpen())
 	{
 		sf::Event event;
@@ -20,11 +22,27 @@ int main()
 				break;
 			}
 		}
-		ImGui::SFML::Update(window, clockImgui.restart());
+		const sf::Time time = clockImgui.restart();
+		ImGui::SFML::Update(window, time);
 		window.clear();
 		/// TESTING ///////////////////////////////////////////////////////////
-		ImGui::Begin("Testing!");
-		ImGui::Button("Test Button");
+		frameTimes.push_back(float(time.asMilliseconds()));
+		frameTimes.erase(frameTimes.begin());
+		ImGui::Begin("Frame Milliseconds", nullptr, 
+			ImGuiWindowFlags_AlwaysAutoResize);
+		ImGui::PlotLines("",
+			[](void* data, int idx)->float 
+			{
+				return (*static_cast<std::vector<float>*>(data))[idx]; 
+			},
+			&frameTimes, frameTimes.size(),
+			0, nullptr, std::numeric_limits<float>::max(),
+			std::numeric_limits<float>::max(), { 400, 150 });
+		ImGui::Text("Min=%i", 
+			int(*std::min_element(frameTimes.begin(), frameTimes.end())));
+		ImGui::SameLine();
+		ImGui::Text("Max=%i", 
+			int(*std::max_element(frameTimes.begin(), frameTimes.end())));
 		ImGui::End();
 		/// ///////////////////////////////////////////////////////////////////
 		ImGui::SFML::Render(window);
